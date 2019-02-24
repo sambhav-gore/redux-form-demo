2f27be911c31840afa743ee935ce4099ea7beb58
Sambhav Gore <sambhavgore@gmail.com>
2019-02-24 20:12:07 +0100

    Add styles to the form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 import { customInput, customSelect } from "./fields";
 import { required, minLength, maxLength } from "../validation/validations";
+import "./RegisterForm.css";
 
 class RegisterForm extends Component {
   render() {
--- src/components/fields/index.js
+++ src/components/fields/index.js
@@ -1,14 +1,34 @@
 import React from "react";
+import cx from "classnames";
 // import ReactJson from "react-json-view";
+
+const getValidityClassName = meta => {
+  if (meta.active) {
+    return;
+  }
+  if (meta.touched && meta.invalid) {
+    return "invalid";
+  }
+  if (meta.touched && meta.valid) {
+    return "valid";
+  }
+};
 
 export const customInput = props => {
   const { label, input, type, meta } = props;
   return (
-    <div>
-      <label>{label}</label>
+    <div
+      className={cx(
+        "custom-input-container",
+        { "flex-row-reverse": type === "checkbox" },
+        { dirty: meta.dirty },
+        getValidityClassName(meta)
+      )}
+    >
       <input {...input} type={type} />
-      {meta.error && meta.touched && (
-        <div style={{ color: "red" }}>{meta.error}</div>
+      <label>{label}</label>
+      {meta.error && meta.touched && !meta.active && (
+        <div className="feedback-text error-text">{meta.error}</div>
       )}
       {/* <ReactJson src={props} /> */}
     </div>
